ef0ff5aadf4ed1b27694713532892496b175f5ed
alex.wheeler <awheeler@totalwebservices.net>
2015-03-31 13:14:11 -0700

    Added additional plugins

--- config.js
+++ config.js
@@ -1,72 +1,94 @@
-﻿/**
+/**
  * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	// %REMOVE_START%
-	config.plugins =
-		'about,' +
-		'a11yhelp,' +
-		'basicstyles,' +
-		'bidi,' +
-		'blockquote,' +
-		'clipboard,' +
-		'colorbutton,' +
-		'colordialog,' +
-		'contextmenu,' +
-		'dialogadvtab,' +
-		'div,' +
-		'elementspath,' +
-		'enterkey,' +
-		'entities,' +
-		'filebrowser,' +
-		'find,' +
-		'flash,' +
-		'floatingspace,' +
-		'font,' +
-		'format,' +
-		'forms,' +
-		'horizontalrule,' +
-		'htmlwriter,' +
-		'image,' +
-		'iframe,' +
-		'indentlist,' +
-		'indentblock,' +
-		'justify,' +
-		'language,' +
-		'link,' +
-		'list,' +
-		'liststyle,' +
-		'magicline,' +
-		'maximize,' +
-		'newpage,' +
-		'pagebreak,' +
-		'pastefromword,' +
-		'pastetext,' +
-		'preview,' +
-		'print,' +
-		'removeformat,' +
-		'resize,' +
-		'save,' +
-		'selectall,' +
-		'showblocks,' +
-		'showborders,' +
-		'smiley,' +
-		'sourcearea,' +
-		'specialchar,' +
-		'stylescombo,' +
-		'tab,' +
-		'table,' +
-		'tabletools,' +
-		'templates,' +
-		'toolbar,' +
-		'undo,' +
-		'wysiwygarea';
-	// %REMOVE_END%
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	
+	config.extraPlugins = 'html5validation,a11yhelp,colordialog,div,fakeobjects,find,flash,forms,iframe,contextmenu,menu,floatpanel,panel,magicline,liststyle,pagebreak,pastefromword,preview,showblocks,smiley,specialchar,table,tabletools,image,enterkey,horizontalrule,maximize,font,richcombo,button,listblock,sourcearea,justify,colorbutton,panelbutton,templates,dialogadvtab,codemirror,widget,lineutils,widgetbootstrap,widgettemplatemenu,filebrowser,popup';
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste';
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+	config.filebrowserBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=flash';
+	config.enterMode = CKEDITOR.ENTER_BR;
+	
 };
 
-// %LEAVE_UNMINIFIED% %REMOVE_LINE%
+config.codemirror = {
+
+		// Set this to the theme you wish to use (codemirror themes)x
+		theme: 'default',
+
+		// Whether or not you want to show line numbers
+		lineNumbers: true,
+
+		// Whether or not you want to use line wrapping
+		lineWrapping: true,
+
+		// Whether or not you want to highlight matching braces
+		matchBrackets: true,
+
+		// Whether or not you want tags to automatically close themselves
+		autoCloseTags: true,
+
+		// Whether or not you want Brackets to automatically close themselves
+		autoCloseBrackets: true,
+
+		// Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
+		enableSearchTools: true,
+
+		// Whether or not you wish to enable code folding (requires 'lineNumbers' to be set to 'true')
+		enableCodeFolding: true,
+
+		// Whether or not to enable code formatting
+		enableCodeFormatting: true,
+
+		// Whether or not to automatically format code should be done when the editor is loaded
+		autoFormatOnStart: true,
+
+		// Whether or not to automatically format code should be done every time the source view is opened
+		autoFormatOnModeChange: true,
+
+		// Whether or not to automatically format code which has just been uncommented
+		autoFormatOnUncomment: true,
+
+		// Define the language specific mode 'htmlmixed' for html including (css, xml, javascript), 'application/x-httpd-php' for php mode including html, or 'text/javascript' for using java script only
+		mode: 'htmlmixed',
+
+		// Whether or not to show the search Code button on the toolbar
+		showSearchButton: true,
+
+		// Whether or not to show Trailing Spaces
+		showTrailingSpace: true,
+
+		// Whether or not to highlight all matches of current word/selection
+		highlightMatches: true,
+
+		// Whether or not to show the format button on the toolbar
+		showFormatButton: true,
+
+		// Whether or not to show the comment button on the toolbar
+		showCommentButton: true,
+
+		// Whether or not to show the uncomment button on the toolbar
+		showUncommentButton: true,
+
+		// Whether or not to show the showAutoCompleteButton button on the toolbar
+		showAutoCompleteButton: true,
+		// Whether or not to highlight the currently active line
+		styleActiveLine: true
+	};
