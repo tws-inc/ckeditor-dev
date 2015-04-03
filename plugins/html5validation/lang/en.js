994d912908774c3859f92c1a191efa4ff4464878
alex.wheeler <awheeler@totalwebservices.net>
2015-04-03 13:51:44 -0700

    Removed html5validation plugin, added as sub-repository Reverted Forms plugin to original state (removed customizations implemented by the html5validation plugin) Added widgetbootstrap as submodule

--- plugins/html5validation/lang/en.js
+++
@@ -1,9 +0,0 @@
-/*
-Copyright (c) 2015, Total Web Services. All rights reserved.
-*/
-CKEDITOR.plugins.setLang('html5validation', 'en', {
-	patternLabel: 'Validation Pattern',
-	requiredLabel: 'Required?',
-	validationTabTitle: 'Form Validation',
-	novalidateLabel: 'Novalidate?'
-});
